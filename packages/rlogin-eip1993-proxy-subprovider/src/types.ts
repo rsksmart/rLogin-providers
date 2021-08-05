437f5b7c4973902f6e9ac91aeb277bbb75fb0189
Diego Grasso <diegograsso@iovlab.org>
2021-08-05 16:26:43 -0300

    Fixed param

--- packages/rlogin-eip1993-proxy-subprovider/src/types.ts
+++ packages/rlogin-eip1993-proxy-subprovider/src/types.ts
@@ -11,7 +11,7 @@ export interface IRLoginEIP1993Provider {
     request(args: { method: 'personal_sign', params: [message: string]  }): Promise<string>
     request(args: { method: 'eth_sendTransaction', params: [transaction:Transaction, blockNumber: string]  }): Promise<any | null>
     request(args: { method: 'eth_estimateGas', params: [transaction:Transaction]  }): Promise<number>
-    request(args: { method: string, params?: any }): Promise<any> //default
+    request(args: { method: string, params?: any[] }): Promise<any> //default
     sendAsync(request: { method: string; params?: any;}, cb: any): void;
     enable(): Promise<unknown>;
     on(method: string): void;
