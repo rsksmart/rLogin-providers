b1f5086e72197db4b7247347c783656191bcccb4
Ilan <ilanolkies@outlook.com>
2021-08-25 16:58:56 -0300

    Add provider options

--- packages/rlogin-dcent-provider/src/index.ts
+++ packages/rlogin-dcent-provider/src/index.ts
@@ -1,13 +1,2 @@
-import { IRLoginEIP1993Provider, RLoginEIP1993Provider } from '@rsksmart/rlogin-eip1193-proxy-subprovider'
-
-export class DCentProvider extends RLoginEIP1993Provider {
-  connect(): Promise<IRLoginEIP1993Provider> {
-    throw new Error('Method not implemented.');
-  }
-  ethSendTransaction(to: string, value: string | number, data: string): Promise<string> {
-    throw new Error('Method not implemented.');
-  }
-  personalSign(message: string): Promise<string> {
-    throw new Error('Method not implemented.');
-  }
-}
+export { DCentProvider } from './DCentProvider'
+export { dcentProviderOptions } from './providerOptions'
