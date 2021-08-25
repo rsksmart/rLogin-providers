1ea67e1cfb486b546c67c591962dc673b71e87fe
Ilan <ilanolkies@outlook.com>
2021-08-25 16:29:40 -0300

    Add D'Cent provider folder

---
+++ packages/rlogin-dcent-provider/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.ts',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'umd',
+    library: "rLoginLedgerProvider",
+    umdNamedDefine: true,
+    globalObject: "this"
+  }
+}
