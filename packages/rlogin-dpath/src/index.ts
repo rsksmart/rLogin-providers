a0a80a9eda95ba5459a6891e307f04867376cb85
Jesse Clark <hello@developerjesse.com>
2022-01-20 15:28:21 +0200

    Fix issues with committed code for tests and linter.

--- packages/rlogin-dpath/src/index.ts
+++ packages/rlogin-dpath/src/index.ts
@@ -13,7 +13,8 @@
  * @returns the first account of standard BIP-44 derviation path forthe given network
  */
 
-export const getDPathByChainId = (chainId: number, index: number = 0, _: boolean): string => {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const getDPathByChainId = (chainId: number, index: number = 0, _?: boolean): string => {
   // if (isLedger && chainId === 31) return `m/44'/1'/0'/0/${index}` // Ledger + RSK Testnet - based on slip-44 - 37310 does not work - user can set standard derivation path using RSK Testnet app
   switch (chainId) {
     case 30: return `m/44'/137'/0'/0/${index}` // RSK Mainnet - based on rskip-57
