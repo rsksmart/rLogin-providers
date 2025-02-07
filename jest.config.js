932feb360d9572d0c7f1148b261b094188e879d3
ronaldsg <ronaldsg20@gmail.com>
2025-02-07 01:50:50 -0500

    update test configuration

--- jest.config.js
+++ jest.config.js
@@ -4,9 +4,27 @@ module.exports = {
   testEnvironment: 'node',
   reporters: ['default', 'jest-junit'],
   testResultsProcessor: 'jest-junit',
-  globals: {
-    'ts-jest': {
-      tsConfig: './packages/tsconfig.settings.json'
-    }
-  }
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        compilerOptions: {
+          preserveConstEnums: true,
+          sourceMap: true,
+          target: "es6",
+          module: "commonjs",
+          moduleResolution: "node",
+          esModuleInterop: true,
+          downlevelIteration: true,
+          declarationMap: true,
+          declaration: true,
+          composite: true,
+          emitDecoratorMetadata: true,
+          experimentalDecorators: true,
+          resolveJsonModule: true
+        },
+        exclude: ["**/test/**/*", "**/lib/**/*"]
+      }
+  ]
+  },
 }
